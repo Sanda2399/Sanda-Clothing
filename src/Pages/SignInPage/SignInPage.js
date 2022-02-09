44504b800f64e20e4a66e2f7c8764ed2262d3d93
Brandon Mitchell <83912617+Sanda2399@users.noreply.github.com>
2022-02-09 12:10:21 -0500

    Implemented Sign Up with email and password with firebase

--- src/Pages/SignInPage/SignInPage.js
+++ src/Pages/SignInPage/SignInPage.js
@@ -1,11 +1,15 @@
 import React from "react";
 
+import './Styling/SignInPage.css';
+
 import SignInComponent from '../../Components/SignIn/SignInComponent';
+import SignUpComponent from '../../Components/SignUp/SignUpComponent';
 
 const SignInPage = () => {
     return (
-        <div className="signin">
+        <div className="signin-signup-page">
             <SignInComponent />
+            <SignUpComponent />
         </div>
     )
 }
